f0caa7e0d0cce49e2e6522dff448cb0147f2edd3
Valentin Lorentz <valentin.lorentz@ens-lyon.org>
2014-06-17 15:42:07 +0200

    Import test262 as git submodules.
    The Mercurial repository that tests/get-test262.sh was downloading from has been
    removed and replace by a Git repository, so we can make use of submodules.

--- tests/test262.sh
+++ tests/test262.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python test262/test262/tools/test262.py \
+python test262/test262/tools/packaging/test262.py \
   --command ./s5-test262 \
   --tests test262/test262/ \
   --full-summary
