9adce18ac35d9df3614f60d89510c9616bf2e954
Valentin Lorentz <valentin.lorentz@ens-lyon.org>
2014-06-25 10:27:12 +0200

    Use paths relative to the current file, not to the current working directory.

--- tests/jsparser.sh
+++ tests/jsparser.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-../bin/js -e "print(JSON.stringify(Reflect.parse(read('$1'),{loc:true}),function(key,value){if(key==='value'&&(value)instanceof(RegExp)){return{re_lit:String(value)}}return(value)},2))"
+BASE_PATH=`dirname $0`/..
+
+$BASE_PATH/bin/js -e "print(JSON.stringify(Reflect.parse(read('$1'),{loc:true}),function(key,value){if(key==='value'&&(value)instanceof(RegExp)){return{re_lit:String(value)}}return(value)},2))"
 exit $?
